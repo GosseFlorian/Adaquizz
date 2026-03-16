33fc8ca4e236472244b31a26f597fe595fb63633
GosseFlorian <floriangosse91@gmail.com>
2026-03-16 10:58:14 +0100

    passage du quiz femmes-scientifique dans le localStorage et création du boutton create new questionnaire

--- src/main.js
+++ src/main.js
@@ -5,5 +5,10 @@ import { showQuestion } from './show-question';
 document.querySelector('#app').innerHTML = `
     <h1>${quiz.title}</h1>
     <button id="start">Démarrer</button>
+    <button id="create-quizz">New quizz</button>
     `
+
+let json = JSON.stringify(quiz)
+localStorage.setItem(`${quiz.title}`,json)
 document.querySelector('#start').addEventListener('click',showQuestion)
+document.querySelector('#create-quizz').addEventListener('click', showCreateQuizz)
