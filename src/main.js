032a01ffad93c64cb5fa07e3cf173eebc48355bd
GosseFlorian <floriangosse91@gmail.com>
2026-03-17 11:11:17 +0100

    ajout d'une boucle afin de montrer tout les questionnaires dans le localStorage pour jouer

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import { showCreateQuizz } from './create-quizz';
 
 document.querySelector('#app').innerHTML = `
     <h1>${quiz.title}</h1>
+    <div id="choose-quizz"></div>
     <button id="start">Démarrer</button>
     <button id="create-quizz">New quizz</button>
     `
-
-// let json = JSON.stringify(quiz)
-// localStorage.setItem(`${quiz.title}`,json)
+    for (let i = 1; i < localStorage.length; i++) {
+        const key = localStorage.key(i)
+        document.querySelector('#choose-quizz').innerHTML +=`
+            <input type="radio" name="quizz" value="${key}" id="radio-${key}">
+            <label for="radio-${key}">${key}</label><br>
+        `
+    }
 document.querySelector('#start').addEventListener('click',showQuestion)
 document.querySelector('#create-quizz').addEventListener('click', showCreateQuizz)
