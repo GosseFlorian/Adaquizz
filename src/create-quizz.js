d1d2c6fa5d1169afeeac6c64b354863928bb2b49
GosseFlorian <floriangosse91@gmail.com>
2026-03-17 10:05:36 +0100

    create a function to create a new quizz with his title, questions (limited to 10) with 4 responses.

---
+++ src/create-quizz.js
@@ -0,0 +1,37 @@
+import './style.css'
+
+export function showCreateQuizz(){
+document.querySelector('#app').innerHTML = `
+    <h1>Creation d'un nouveau quizz !</h1>
+    <label for="title">Titre du nouveau quizz :</label>
+    <input type="text" id="title"/>
+    <label for="number-question">Nombre de question (1 à 10) :</label>
+    <input type="number" id="number-question" min="1" max="10"/>
+    <div id="NewQuestionAndChoice"></div>
+    <button id="CreateNewQuizz">Valider</button> 
+    `
+
+    let numberQuestion = document.querySelector('#number-question')
+    let NewQuestionAndChoice = document.querySelector('#NewQuestionAndChoice')
+
+    numberQuestion.addEventListener("input", () => {
+        let number = numberQuestion.value;
+
+        NewQuestionAndChoice.innerHTML = "";
+
+        for(let i = 1; i <= number; i++){
+            NewQuestionAndChoice.innerHTML += `
+            <input class="question" type="text" placeholder="Nom de la question ${i}"/>
+                <div class="newQuestion">
+                    <input class="choice" type="text" placeholder="Reponse 1"/>
+                    <input class="choice" type="text" placeholder="Reponse 2"/>
+                    <input class="choice" type="text" placeholder="Reponse 3"/>
+                    <input class="choice" type="text" placeholder="Reponse 4"/>
+                </div>
+            `
+        }
+    })
+    document.querySelector('#CreateNewQuizz').addEventListener('click', async() =>{
+            changeJson()
+    })
+}
