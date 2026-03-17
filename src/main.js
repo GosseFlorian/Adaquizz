967526a0503c24c13fe1915d5342727efe8d75f6
GosseFlorian <floriangosse91@gmail.com>
2026-03-17 11:53:37 +0100

    ajout de petit commentaire memo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import { acceuil } from "./acceuil";
-
+// for (let index = 0; index < localStorage.length; index++) {
+//     console.log(localStorage.key(index))
+//     console.log(localStorage.getItem(`${localStorage.key(index)}`))
+// }
+//
+//     {
+//   "title": "Mon quiz",
+//   "questions": [
+//     {
+//       "question": "Question 1 ?",
+//       "options": ["rep1", "rep2", "rep3", "rep4"]
+//       "correctIndex":1,
+//     }
+//   ]
+// }
 acceuil()
